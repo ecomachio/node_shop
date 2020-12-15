cc069cdf15d1f07cee76f8c6edb394fff6903c0f
Edian <ediancomachio@hotmail.com>
2020-12-14 23:05:33 -0300

    get rid of sql on app js

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ const orderRoutes = require('./routes/order')
 const exceptionsController = require('./controllers/exceptions');
 
 const sequelize = require('./utils/database');
-const Product = require('./models/product');
-const User = require('./models/User');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -28,8 +22,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(async (req, res, next) => {
-    req.user = await User.findByPk(1);
-    console.log(req.user);
+    // req.user = await User.findByPk(1);
+    // console.log(req.user);
     next();
 })
 
@@ -38,35 +32,3 @@ app.use('/shop', shopRoutes)
 app.use('/order', orderRoutes)
 
 app.use(exceptionsController.pageNotFound)
-
-// Relations
-Product.belongsTo(User);
-User.hasMany(Product)
-
-User.hasOne(Cart);
-Cart.hasMany(CartItem);
-
-Product.belongsToMany(Cart, { through: CartItem });
-Cart.belongsToMany(Product, { through: CartItem });
-
-User.hasMany(Order)
-Order.belongsTo(User)
-Order.belongsToMany(Product, { through: OrderItem })
-
-sequelize.sync(
-   //     { force: true }
-)
-    .then(() => User.findByPk(1))
-    .then(user => {
-        if (!user) {
-            return User.create({
-                name: 'Edian', email: 'ediancomachio@hotmail.com'
-            });
-        } else {
-            return user;
-        }
-    }).then((user) => {
-        user.createCart();
-    })
-    .then(() => app.listen(3000))
-    .catch(err => console.error(err));
