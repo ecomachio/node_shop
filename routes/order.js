b4bf4a01f9d207211916c1977e4d54d53d3bf15a
Edian <ediancomachio@hotmail.com>
2020-12-14 22:15:21 -0300

    just a test

--- routes/order.js
+++ routes/order.js
@@ -7,6 +7,6 @@ const Router = express.Router()
 Router.post('/create-order', orderController.postOrder)
 
 Router.get('/checkout', orderController.getCheckout)
-//Router.get('/orders', orderController.getOrders)
+
 
 module.exports = Router;
