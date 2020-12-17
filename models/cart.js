80681e83b595a80d07074f4cff064c87e689e3d6
Edian <ediancomachio@hotmail.com>
2020-12-16 23:59:07 -0300

    comment sequelize models

--- models/cart.js
+++ models/cart.js
@@ -1,17 +1,17 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = require('../utils/database');
+// const sequelize = require('../utils/database');
 
-const Cart = sequelize.define('cart', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    totalPrice: {
-        type: Sequelize.DOUBLE
-    }
-});
+// const Cart = sequelize.define('cart', {
+//     id: {
+//         type: Sequelize.INTEGER,
+//         autoIncrement: true,
+//         allowNull: false,
+//         primaryKey: true,
+//     },
+//     totalPrice: {
+//         type: Sequelize.DOUBLE
+//     }
+// });
 
-module.exports = Cart
+// module.exports = Cart
