ba1ae98d296606128de1ca6124d93428beb149ac
Edian <ediancomachio@hotmail.com>
2020-12-16 23:59:20 -0300

    comment squelize model

--- models/user.js
+++ models/user.js
@@ -1,24 +1,24 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = require('../utils/database');
+// const sequelize = require('../utils/database');
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
+// const User = sequelize.define('user', {
+//     id: {
+//         type: Sequelize.INTEGER,
+//         autoIncrement: true,
+//         allowNull: false,
+//         primaryKey: true,
+//     },
 
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
+//     name: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//     },
+//     email: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//     },
 
-});
+// });
 
-module.exports = User
+// module.exports = User
